d6c57c9b257327aa6884763d11347b89d12e5fa9
crimsondesert1985-png <crimsondesert1985@gmail.com>
2026-08-01 17:35:15 -0800

    Sprint 41: register Ember queue APIs

--- BrokerBeacon_AI_Phase2/wsgi.py
+++ BrokerBeacon_AI_Phase2/wsgi.py
@@ -6,6 +6,7 @@ from ai_ops_api import install_ai_ops
 from control_tower_ux import install_control_tower_ux
 from discovery_ops_api import install_discovery_ops
 from ember_automation_api import install_ember_automation
+from ember_queue_api import install_ember_queue_api
 from ember_status_api import install_ember_status_api
 from ember_worker import install_ember_worker
 from national_data_center import install_national_data_center
@@ -21,6 +22,7 @@ install_discovery_ops(app, DB)
 install_ai_ops(app, DB)
 install_ember_automation(app, DB)
 install_ember_status_api(app, DB)
+install_ember_queue_api(app, DB)
 install_sprint38_api(app, DB)
 install_sprint39_api(app, DB)
 install_national_data_center(app)
